443ec286bb69c2a7aa2ce9badaa5a1d71c0f49ad
chxoe <github-kr9kg4d4@0k.rip>
2025-03-31 04:47:03 +0000

    Dynamic nameserver IP address for local networking

--- config/nixos.nix
+++ config/nixos.nix
@@ -1,12 +1,6 @@
-{ configDir, machineConfig, selfDir, userConfig, config, lib, pkgs, ... }:
-
-# let userConfig = import "${config.age.secrets.user.path}";
-# in 
-{	
+{ configDir, machineConfig, selfDir, userConfig, config, lib, pkgs, ... }: {	
 	imports = [ "${configDir}/hardware-configuration.nix" ];
-	
 	boot.loader.grub = { enable = true; device = "/dev/sda"; };
-	
 	networking.hostName = machineConfig.hostname or "nixos";
 	networking.interfaces.ens3.ipv4.addresses = lib.mkIf (machineConfig.staticIp != null) [
 		{ address = machineConfig.staticIp; prefixLength = 24; }
@@ -15,7 +9,7 @@
 		"local" = { interface = "ens3"; address = "10.0.0.200"; };
 	}."${machineConfig.network}";
 	networking.nameservers = {
-		"local" = [ "10.0.0.202" ];
+		"local" = [ (import "${selfDir}/machine/technitium/technitium.nix").staticIp ];
 	}."${machineConfig.network}";
 	networking.networkmanager.enable = true;
 	networking.firewall.enable = false;
@@ -30,4 +24,3 @@
 	services.openssh.enable = true;
 	system.stateVersion = "24.11";
 }
-
