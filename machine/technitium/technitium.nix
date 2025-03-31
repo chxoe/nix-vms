cd684a3fee60e924866ed4856da3f516c755fd02
chxoe <github-kr9kg4d4@0k.rip>
2025-03-31 05:34:06 +0000

    Make config available to passthrough

--- machine/technitium/technitium.nix
+++ machine/technitium/technitium.nix
@@ -3,11 +3,11 @@
 	staticIp = "10.0.0.202";
 	network = "local";
 	##
-	passthrough = pkgs: config: {
+	passthrough = {self}: {pkgs, config, ...}: {
 			services.technitium-dns-server = {
 				enable = true;
-				package = pkgs.callPackage "${config}/custom-package/technitium-dns-server-latest.nix" {
-					technitium-dns-server-library = pkgs.callPackage "${config}/custom-package/technitium-dns-server-library-latest.nix" {};
+				package = pkgs.callPackage "${self}/custom-package/technitium-dns-server-latest.nix" {
+					technitium-dns-server-library = pkgs.callPackage "${self}/custom-package/technitium-dns-server-library-latest.nix" {};
 				};
 			};
 	};
