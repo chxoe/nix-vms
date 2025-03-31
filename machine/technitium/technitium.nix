c4d1b96a52492dc9fb9b8a83889a11bb9447f3b4
chxoe <github-kr9kg4d4@0k.rip>
2025-03-31 04:28:22 +0000

    Move technitium library reference into callPkgs override

--- machine/technitium/technitium.nix
+++ machine/technitium/technitium.nix
@@ -3,15 +3,12 @@
 	staticIp = "10.0.0.202";
 	network = "local";
 	##
-	passthrough = pkgs: config:
-		let
-			customPkgs = pkgs // {
-				technitium-dns-server-library = pkgs.callPackage "${config}/custom-package/technitium-dns-server-library-latest.nix" {};
-			};
-		in {
+	passthrough = pkgs: config: {
 			services.technitium-dns-server = {
 				enable = true;
-				package = customPkgs.callPackage "${config}/custom-package/technitium-dns-server-latest.nix" {};
+				package = pkgs.callPackage "${config}/custom-package/technitium-dns-server-latest.nix" {
+					technitium-dns-server-library = pkgs.callPackage "${config}/custom-package/technitium-dns-server-library-latest.nix" {};
+				};
 			};
-		};
+	};
 }
