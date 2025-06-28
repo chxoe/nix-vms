ce0e1741bd338ccec0c4fa8f7d5f9d149e90c27f
chxoe <github-kr9kg4d4@0k.rip>
2025-06-28 22:44:31 +0000

    Misc updates for jumpbox-public

--- config/generate-tailscale-acls.sh
+++ config/generate-tailscale-acls.sh
@@ -4,4 +4,6 @@ jq -n -f $1 \
 	--arg jumpbox "$(tailscale ip -4 jumpbox)" \
 	--arg caddy   "$(tailscale ip -4 caddy)" \
 	--arg zitadel "$(tailscale ip -4 zitadel)" \
-	--arg misc_trusted "$(tailscale ip -4 misc-trusted)"
+	--arg misc_trusted "$(tailscale ip -4 misc-trusted)" \
+	--arg jumpbox_public "$(tailscale ip -4 jumpbox-public)" \
+	--arg caddy_public "$(tailscale ip -4 caddy-public)"
