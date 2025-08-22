24e1cf5a5389ac192e39b449b0929266f1cabc8b
chxoe <github-kr9kg4d4@0k.rip>
2025-08-22 18:08:51 +0000

    Fix another syntax problem

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
 								rust-service = rust-service (system-from-config machineConfig);
 								java-jar-service = java-jar-service (machinePkgs);
 								drive = drive;
-								aggregator = aggregator.outputs.defaultPackage."$(system-from-config machineConfig)";
+								aggregator = aggregator.outputs.defaultPackage.${system-from-config machineConfig};
 							}) else ({...}:{}));
 						inherit inputs;
 					};
