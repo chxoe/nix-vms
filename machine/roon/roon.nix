5ce266f90afa8be41c6ad69377ea6de40d59dd47
chxoe <github-kr9kg4d4@0k.rip>
2025-09-24 09:06:56 -0400

    Brainstorming for host to nixos migration

--- machine/roon/roon.nix
+++ machine/roon/roon.nix
@@ -2,17 +2,17 @@
 	hostname = "roon";
 	network = "local";
 	staticIp = "10.0.0.201";
-	##
+	RAM = "4G";
+	CPUthreads = 4;
+	LANports = [ 50000 ];
 	unfree = [ "roon-server-earlyaccess" ];
 	passthrough = {self, ...}: {pkgs, config, ...}: {
-		
-		# For use as a build system... temporary!
-		boot.binfmt.emulatedSystems = ["aarch64-linux"];
-		# End temporary section.
-		
 		services.roon-server = {
 			package = pkgs.callPackage "${self}/custom-package/roon-server-earlyaccess.nix" {};
 			enable = true;
 		};
 	};
+	backupScript = ''
+		$backupDirs /music /backup
+	'';
 }
