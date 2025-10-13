6b07142bb37d03bfa594477418fce996751b1238
chxoe <github-kr9kg4d4@0k.rip>
2025-10-13 20:15:25 +0000

    Disable IPv6 for Roon ARC

--- machine/roon/roon.nix
+++ machine/roon/roon.nix
@@ -10,6 +10,9 @@
 		boot.binfmt.emulatedSystems = ["aarch64-linux"];
 		# End temporary section.
 		
+		# Temporary workaround for issues with EA IPv6 changes and my network setup
+		networking.enableIPv6 = false;
+		
 		services.roon-server = {
 			package = pkgs.callPackage "${self}/custom-package/roon-server-earlyaccess.nix" {};
 			enable = true;
