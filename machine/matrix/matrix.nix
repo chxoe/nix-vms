44c0dda8c5ec9ea77f92642572084d473ee7ce59
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:12 +0000

    Fix matrix overrides

--- machine/matrix/matrix.nix
+++ machine/matrix/matrix.nix
@@ -2,17 +2,18 @@
 	hostname = "matrix";
 	network = "local";
 	staticIp = "10.0.0.203";
-	passthrough = {private, modules, ...}: {pkgs, ...}@moduleInputs:
-		modules.matrix-stack {
-			nginx.acme-email = private.emails."matrix-acme";
-			element.domain = private.domains.element;
-			matrix = {
-				domain = private.domains.matrix;
-				registration-secret = private.matrixRegistrationSecret;
+	passthrough = {private, modules, ...}: {pkgs, lib, ...}@moduleInputs:
+		lib.recursiveUpdate
+			(modules.matrix-stack {
+				nginx.acme-email = private.emails."matrix-acme";
+				element.domain = private.domains.element;
+				matrix = {
+					domain = private.domains.matrix;
+					registration-secret = private.matrixRegistrationSecret;
+				};
+			} moduleInputs)
+			{
+				networking.enableIPv6 = false;
+				networking.firewall.enable = pkgs.lib.mkForce true;
 			};
-		} moduleInputs
-		// {
-			networking.enableIPv6 = false;
-			networking.firewall.enable = pkgs.lib.mkForce true;
-		};
 }
