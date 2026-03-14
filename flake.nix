f8f62eb57cc66abc6107a8efe64226addc76d4bd
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:10 +0000

    Experimental rework of rust service code

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
 	description = "System configuration helper";
-	inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	inputs.private.url = "git+ssh://git@github.com/chxoe/nix-vms-private.git?ref=main";
-	inputs.naersk.url = "github:nix-community/naersk";
-	outputs = { self, nixpkgs, private, naersk }@inputs:
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		private.url = "git+ssh://git@github.com/chxoe/nix-vms-private.git?ref=main";
+		naersk.url = "github:nix-community/naersk";
+		drive.url = "git+ssh://git@github.com:chxoe/drive.git?ref=main";
+	};
+	outputs = { self, nixpkgs, private, naersk, drive }@inputs:
 		let
 			system-from-config = machineConfig: machineConfig.system or "x86_64-linux";
 			machine-pkgs-from-config = machineConfig:
@@ -12,6 +15,35 @@
 					system = system;
 					config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.legacyPackages."${system}".lib.getName pkg) machineConfig.unfree or [];
 				};
+			rust-service = {system}: {name, source, executable}: {
+				systemd.services."${name}" = let
+					derivation = naersk.ouputs.lib."${system}".buildPackage { src = source; };
+				in {
+					enable = true;
+					
+					# Auto start the service on boot.
+					wantedBy = [ "multi-user.target" ];
+					
+					# See man systemd.exec
+					serviceConfig = {
+						# Run the given executable, writing logs out to the log directory (see below). 
+						ExecStart = "${self}/util/systemd-exec.sh ${derivation}/bin/${executable}";
+						
+						# For persistent data. Accessible via $STATE_DIRECTORY, location = /var/lib/${name}
+						StateDirectory = name;
+						
+						# For logs, used by util/systemd-exec.sh. Accessible via $LOGS_DIRECTORY, location = /var/log/${name}
+						LogsDirectory = name;
+						
+						# Use the location of the source code (read-only) in the Nix store as the working directory.
+						WorkingDirectory = "${source}";
+						
+						# Allow services to bind to low # ports
+						AmbientCapabilities = "cap_net_bind_service";
+						CapabilityBoundingSet = "cap_net_bind_service";
+					};
+				};
+			};
 			system-from-name = machine: 
 				let
 					userConfig = import "${self}/config/user.nix";
@@ -27,7 +59,8 @@
 						passthrough = (if machineConfig?passthrough then (machineConfig.passthrough {
 								self = self;
 								private = private;
-								naersk = let system = system-from-config machineConfig; in naersk.outputs.lib."${system}";
+								rust-service = rust-service (system-from-config machineConfig);
+								inherit drive;
 							}) else ({...}:{}));
 						inherit inputs;
 					};
--- machine/misc-trusted/misc-trusted.nix
+++ machine/misc-trusted/misc-trusted.nix
@@ -2,22 +2,11 @@
 	hostname = "misc-trusted";
 	network = "tailscale";
 	##
-	passthrough = {naersk, self, ...}: {pkgs, config, ...}: {
-		systemd.services.drive = let
-			driveSource = builtins.fetchGit { "url" = "git@github.com:chxoe/drive.git"; "ref" = "main"; "rev" = "1ddd9538dd89e820d1c23e2f7b870973d7bc5801"; };
-			driveDerivation = naersk.buildPackage { src = "${driveSource}"; };
-		in {
-			enable = true;
-			path = [ ];
-			serviceConfig = {
-				ExecStart = "${self}/util/systemd-exec.sh ${driveDerivation}/bin/server";
-				AmbientCapabilities = "cap_net_bind_service";
-				CapabilityBoundingSet = "cap_net_bind_service";
-				StateDirectory = "drive";
-				LogsDirectory = "drive";
-				WorkingDirectory = "${driveSource}";
-			};
-			wantedBy = [ "multi-user.target" ];
+	passthrough = {rust-service, drive, ...}: {pkgs, config, ...}: {
+		inherit rust-service {
+			name = "drive";
+			source = ${drive};
+			executable = "server";
 		};
 	};
 }
