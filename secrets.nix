4185d93ae73a8183f2c4324fadf0c20b8a2277bc
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:08 +0000

    Add caddy SSH key to secrets

--- secrets.nix
+++ secrets.nix
@@ -2,6 +2,7 @@ let
 	keys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIICGvtbpmVRVRpQsIXwq9nE8SwN+XM+j3zYw1GTSoIw0 root@roon"
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtLZNibBf9ImykRP/V71jjNkT/obctCh5N76Z0S0BvT root@technitium"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEQp9eG8q6NV/LWJsP9XaMO0SN14oBd5fPXUl772jMf root@caddy"
 	];
 in {
 	"config/domains.age".publicKeys = keys;
