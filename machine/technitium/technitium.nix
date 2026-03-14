7ae77be63f0a54d11ef50c00130e0af79efc3c4c
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:08 +0000

    Fix misplaced let

--- machine/technitium/technitium.nix
+++ machine/technitium/technitium.nix
@@ -3,14 +3,15 @@
 	staticIp = "10.0.0.202";
 	network = "local";
 	##
-	passthrough = pkgs: config: {
+	passthrough = pkgs: config:
 		let
 			pkgs = pkgs // {
 				technitium-dns-server-library = pkgs.callPackage "${config}/custom-package/technitium-dns-server-library-latest.nix" {};
 			};
-		in services.technitium-dns-server = {
-			enable = true;
-			package = pkgs.callPackage "${config}/custom-package/technitium-dns-server-latest.nix" {};
+		in {
+			services.technitium-dns-server = {
+				enable = true;
+				package = pkgs.callPackage "${config}/custom-package/technitium-dns-server-latest.nix" {};
+			};
 		};
-	};
 }
