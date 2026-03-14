7deb70994605209f0c0946c29263dcaae546012d
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:12 +0000

    Add postgres to aggregator

--- machine/aggregator/aggregator.nix
+++ machine/aggregator/aggregator.nix
@@ -8,5 +8,16 @@
 			source = aggregator;
 			jar = "${private.aggregatorJarFile}";
 		};
+		services.postgresql = {
+			enable = true;
+			package = pkgs.postgresql_15;
+			dataDir = "/postgresql/data";
+			authentication = pkgs.lib.mkOverride 10 ''
+				#type	db	user			method
+				local	all	all			trust
+				host	all	all	127.0.0.1/32	trust
+				host	all	all	::1/128		trust
+			'';
+		};
 	};
 }
