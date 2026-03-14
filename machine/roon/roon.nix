1a3ef214d7e4ac713884479d8517beef1056fcac
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:10 +0000

    Add caddy-public

--- machine/roon/roon.nix
+++ machine/roon/roon.nix
@@ -5,6 +5,11 @@
 	##
 	unfree = [ "roon-server-earlyaccess" ];
 	passthrough = {self, ...}: {pkgs, config, ...}: {
+		
+		# For use as a build system... temporary!
+		boot.binfmt.emulatedSystems = ["aarch64-linux"];
+		# End temporary section.
+		
 		services.roon-server = {
 			package = pkgs.callPackage "${self}/custom-package/roon-server-earlyaccess.nix" {};
 			enable = true;
