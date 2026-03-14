8ead69ac4493714d28143ea0acef136c15787f5e
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-14 00:12:10 +0000

    WIP jumpbox

--- machine/jumpbox-public/jumpbox-public.nix
+++ machine/jumpbox-public/jumpbox-public.nix
@@ -1,4 +1,5 @@
 {
+	system = "aarch64-linux";
 	hostname = "jumpbox-public";
 	network = "external";
 	##
@@ -13,22 +14,22 @@
 			extraSetFlags = [ "--ssh" "--advertise-exit-node" ];
 		};
 		networking.nftables.enable = true;
-		networking.nftables.ruleset = ''
-			table ip nat {
-				chain PREROUTING {
-					iifname "ens5" \
-					ip protocol tcp \
-					tcp dport { 80, 443 } \
-					dnat to caddy-public
-				}
-				chain POSTROUTING {
-					oifname "tailscale0" \
-					ip protocol tcp \
-					ip daddr caddy-public \
-					tcp dport { 80, 443 } \
-					masquerade
-				}
-			}
-		'';
+	#	networking.nftables.ruleset = ''
+	#		table ip nat {
+	#			chain PREROUTING {
+	#				iifname "ens5" \
+	#				ip protocol tcp \
+	#				tcp dport { 80, 443 } \
+	#				dnat to caddy-public
+	#			}
+	#			chain POSTROUTING {
+	#				oifname "tailscale0" \
+	#				ip protocol tcp \
+	#				ip daddr caddy-public \
+	#				tcp dport { 80, 443 } \
+	#				masquerade
+	#			}
+	#		}
+	#	'';
 	};
 }
