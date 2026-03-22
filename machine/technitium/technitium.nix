4c9a04b2f966f4cf4e61d9fdc098f20854e06148
chxoe <205576386+chxoe@users.noreply.github.com>
2026-03-21 21:24:31 -0400

    Update technitium.nix

--- machine/technitium/technitium.nix
+++ machine/technitium/technitium.nix
@@ -6,9 +6,9 @@
 	passthrough = {self,...}: {pkgs, config, ...}: {
 			services.technitium-dns-server = {
 				enable = true;
-				package = pkgs.callPackage "${self}/custom-package/technitium-dns-server-latest.nix" {
-					technitium-dns-server-library = pkgs.callPackage "${self}/custom-package/technitium-dns-server-library-latest.nix" {};
-				};
+				# package = pkgs.callPackage "${self}/custom-package/technitium-dns-server-latest.nix" {
+				# 	technitium-dns-server-library = pkgs.callPackage "${self}/custom-package/technitium-dns-server-library-latest.nix" {};
+				# };
 			};
 	};
 }
